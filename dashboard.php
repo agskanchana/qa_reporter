245811e5a523db899d45539a042d3f81adfce771
agskanchana <sameerakanchana@live.com>
2025-03-01 17:06:45 +0530

    updated version

--- dashboard.php
+++ dashboard.php
@@ -307,7 +307,7 @@ require_once 'includes/header.php';
 
             <main class="col-md-12">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h1 class="h2">Dashboard</h1>
+                    <h1 class="h2">Dashboard test</h1>
                     </div>
                 <div class="row">
 
